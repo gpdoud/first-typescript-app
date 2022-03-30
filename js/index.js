547245f7edbf972f01c1816f88844315ccb6aeae
Greg Doud <gdoud@gmail.com>
2022-03-30 14:20:41 -0400

    final commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const helloWorld = () => {
+    console.log("Hello Typescript World!");
+};
+helloWorld();
+const add = (x, y) => {
+    console.log(x + y);
+};
+add(2, 3);
+add(127, 834);
+let a = 44;
+let b = 6;
+let c = 888;
---
+++ ts/index.ts
@@ -0,0 +1,12 @@
+const helloWorld = () => {
+    console.log("Hello Typescript World!");
+}
+helloWorld();
+const add = (x: number, y: number): void => {
+    console.log(x + y);
+}
+add(2, 3);
+add(127, 834);
+let a = 44;
+let b = 6;
+let c = 888;
